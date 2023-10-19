0bf1fcb31fdf8df193443a7e5e32b440011a4fc9
Daniel Tan <danieltan380@gmail.com>
2023-10-19 12:42:00 -0700

    Refactor getUserWithEmail function to use SQL query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -17,15 +17,17 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+const getUserWithEmail = (email) => {
+  const queryString = `SELECT * FROM users WHERE email = $1;`;
+  const values = [email];
+
+  return pool.query(queryString, values)
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error(err.message);
+  })
 };
 
 /**
