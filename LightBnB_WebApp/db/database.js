c9666d735e31cf93142cf308b077bb4437e9aece
Daniel Tan <danieltan380@gmail.com>
2023-10-19 19:40:17 -0700

    Refactor addProperty function to use SQL queries

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -170,10 +170,26 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+  INSERT INTO properties (
+    title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url,
+    cover_photo_url, street, country, city, province, post_code, owner_id
+    ) 
+    VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+      ) 
+      RETURNING *
+      `;
+  const values = Object.values(property);
+
+  return pool.query(queryString, values)
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error(err.message);
+  })
 };
 
 module.exports = {
